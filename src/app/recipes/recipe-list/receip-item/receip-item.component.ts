142477b76c6bf18b697d858e2a4b1f494d4673f3
thushara <thushara499@gmail.com>
2020-11-09 18:03:50 +0800

    Initial commit

---
+++ src/app/recipes/recipe-list/receip-item/receip-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receip-item',
+  templateUrl: './receip-item.component.html',
+  styleUrls: ['./receip-item.component.css']
+})
+export class ReceipItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

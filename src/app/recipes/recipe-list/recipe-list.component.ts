75436146b5434e437fc84e0cf031559173b9fa44
thushara <thushara499@gmail.com>
2020-11-14 11:11:33 +0800

    before data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 
+    'https://hips.hearstapps.com/hmg-prod/images/190430-cheesy-bacon-ranch-chicken-226-1556829090.jpg'),
+    new Recipe('Test Recipe', 'This is simply a test', 
+    'https://hips.hearstapps.com/hmg-prod/images/190430-cheesy-bacon-ranch-chicken-226-1556829090.jpg')
+  ];
 
   constructor() { }
 
